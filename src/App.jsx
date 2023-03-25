61bf00d70110ea43ff2da06692270f1a29215750
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-03-26 06:47:41 +0800

    adding files

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import WindowTracker from './components/WindowTracker';
+
+const App = () => {
+
+  const [show, setShow] = React.useState(true)
+
+  function toggle() {
+    setShow(prevShow => !prevShow)
+}
+
+  return (
+    <div className="container">
+      <button onClick={toggle}>
+          Toggle WindowTracker
+      </button>
+      {show && <WindowTracker />}
+    </div>
+  )
+}
+
+export default App
