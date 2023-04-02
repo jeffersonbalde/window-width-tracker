4143819ffec4bc332ecef1eb9881c59a5efa38a9
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-04-02 09:03:47 +0800

    adding useLayoutEffect feautures

--- src/components/WindowTracker.jsx
+++ src/components/WindowTracker.jsx
@@ -3,19 +3,32 @@ import React from 'react'
 const WindowTracker = () => {
   const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
 
-  React.useEffect(() => {
+//   React.useEffect(() => {
+//     function watchWidth() {
+//         console.log("Setting up...")
+//         setWindowWidth(window.innerWidth)
+//     }
+    
+//     window.addEventListener("resize", watchWidth)
+    
+//     return function() {
+//         console.log("Cleaning up...")
+//         window.removeEventListener("resize", watchWidth)
+//     }
+// }, [])
+  React.useLayoutEffect(() => {
     function watchWidth() {
-        console.log("Setting up...")
-        setWindowWidth(window.innerWidth)
-    }
-    
-    window.addEventListener("resize", watchWidth)
-    
-    return function() {
-        console.log("Cleaning up...")
-        window.removeEventListener("resize", watchWidth)
-    }
-}, [])
+      console.log("Setting up...")
+      setWindowWidth(window.innerWidth)
+  }
+  
+  window.addEventListener("resize", watchWidth)
+  
+  return function() {
+      console.log("Cleaning up...")
+      window.removeEventListener("resize", watchWidth)
+  }
+  })
   
   return (
     <h1>Window width: {windowWidth}</h1>
